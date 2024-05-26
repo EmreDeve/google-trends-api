28b8e80f4a4aae8970088808f8db356e3d15bd6a
EmreDeve <emredev7@gmail.com>
2024-05-27 00:42:42 +0300

    add realtime endpoint

--- src/endpoint.js
+++ src/endpoint.js
@@ -2,6 +2,15 @@ import express from 'express'
 
 const app = express()
 
+app.get('/realtime', async (req, res) => {
+  const geo = req.query.geo
+  const category = req.query.category
+  return res.json({
+    geo: geo,
+    category: category
+  })
+})
+
 app.get('/poll', async (req, res) => {
     console.log("heart beat")
     return res.json({
