8810377f5237659975035c1895d2e4a1c386464d
EmreDeve <emredev7@gmail.com>
2024-05-27 03:34:40 +0300

    fix v8

--- src/endpoint.js
+++ src/endpoint.js
@@ -4,14 +4,14 @@ import googleTrends from '../lib/google-trends-api.min.js';
 const app = express()
 
 app.get('/realtime', async (req, res) => {
-  try {
   const geo = req.query.geo
   const category = req.query.category
 
-    googleTrends.realTimeTrends({
-      geo: geo,
-      category: category,
-      }, function(err, results) {
+  googleTrends.realTimeTrends({
+    geo: geo,
+    category: category,
+    }, function(err, results) {
+      try {
         if (err) {
           return res.json({
             error: 1
@@ -22,12 +22,12 @@ app.get('/realtime', async (req, res) => {
             results: results
           })
         }
-      })
-  } catch (err) {
-    return res.json({
-      error: 1
+      } catch (error) {
+        return res.json({
+          error: 1
+        })
+      }
     })
-  }
 })
 
 app.get('/poll', async (req, res) => {
